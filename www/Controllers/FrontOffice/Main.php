0fe3c5d9232fd1c189eddd48056fb8d63f9a0226
Florddev <florian.defay@protonmail.com>
2023-12-12 12:00:42 +0100

    wip

--- www/Controllers/FrontOffice/Main.php
+++ www/Controllers/FrontOffice/Main.php
@@ -24,6 +24,8 @@ class Main
 
         echo "<pre>";
         print_r($myCategory);
+        print_r($myCategory);
+        print_r($myCategory);
         echo "</pre>";
 
         new View("FrontOffice/Main/home", $route["template"]);
