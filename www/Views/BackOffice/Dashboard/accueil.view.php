43239027abb5b6e005a69a44a23de1c5eb2ca030
ThamiEngineering <developer@thami.fr>
2024-01-21 17:09:24 +0100

    wip

--- www/Views/BackOffice/Dashboard/accueil.view.php
+++ www/Views/BackOffice/Dashboard/accueil.view.php
@@ -1,4 +1,43 @@
-<div class="main-dashboard">
+<div class="header-main-dashboard">
     <h1><span>Accueil</span> Dashboard</h1>
     <h2>ADMIN PANEL</h2>
+</div>
+
+<div class="stats-container">
+  <div class="stat-card purple">
+    <div class="stat-icon">
+        <i class="ri-line-chart-line"></i>
+    </div>
+    <div class="stat-info">
+      <h3>Nombre de Visites</h3>
+      <p>729 / visites</p>
+    </div>
+  </div>
+  <div class="stat-card pink">
+    <div class="stat-icon">
+        <i class="ri-folder-user-line"></i>
+    </div>
+    <div class="stat-info">
+      <h3>Nombre d'Inscrits</h3>
+      <p>253 / utilisateurs</p>
+    </div>
+  </div>
+  <div class="stat-card yellow">
+    <div class="stat-icon">
+        <i class="ri-news-line"></i>
+    </div>
+    <div class="stat-info">
+      <h3>Nombre d'Articles</h3>
+      <p>27 / articles</p>
+    </div>
+  </div>
+  <div class="stat-card blue">
+    <div class="stat-icon">
+        <i class="ri-chat-1-line"></i>
+    </div>
+    <div class="stat-info">
+      <h3>Nb. de Commentaires</h3>
+      <p>1369 / messages</p>
+    </div>
+  </div>
 </div>
