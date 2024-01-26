6dbcb545bd48fa235c7d257e78d5eb22edf3437f
Victor Jacquin <victorjacquin.dev@gmail.com>
2024-01-26 14:44:37 +0100

    phpMailer

--- www/Controllers/FrontOffice/Security.php
+++ www/Controllers/FrontOffice/Security.php
@@ -2,7 +2,13 @@
 
 namespace App\Controllers\FrontOffice;
 
+
 use App\Core\Routing;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
 use App\Core\View;
 
 class Security
@@ -18,6 +24,39 @@ class Security
     {
 
         $myView = new View("FrontOffice/Security/login", $route["template"]);
+
+        $mail = new PHPMailer(true); 
+
+        try {
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com'; 
+            $mail->SMTPAuth   = true;
+            $mail->Username   = "email"; // Activier double authentification sur compte
+            $mail->Password   = "password"; // MDP à créer sur compte google pour application
+            $mail->Port       = 587; 
+            $mail->SMTPSecure = 'tls';
+$mail->SMTPOptions = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ],
+];
+
+            // Recipients
+            $mail->setFrom('FromEmail', 'PageBuilder');
+            $mail->addAddress('TOEmail', 'User'); // Add a recipient
+
+            // Content
+            $mail->isHTML(true); // Set email format to HTML
+            $mail->Subject = 'Subject';
+            $mail->Body    = 'body';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
     public function register($route): void
     {
